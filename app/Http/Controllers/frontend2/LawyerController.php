f3b0f454ff781750960cacf4d94dbb93666ca967
domalasherpa <domalasherpa9813@gmail.com>
2023-10-23 08:40:05 +0545

    file name changed to class name

---
+++ app/Http/Controllers/frontend2/LawyerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\frontend2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Citizen;
+use App\Models\Lawyer;
+use Illuminate\Http\Request;
+
+class LawyerController extends Controller
+{
+    public function lawyer()
+    {
+        $citizens = Citizen::all();
+        return view('lawyer', compact('citizens'));
+    }
+}
+
